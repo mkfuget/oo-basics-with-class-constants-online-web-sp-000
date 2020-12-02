4e57008bdc73e81b2e678ae4e3ebe41345c307c8
Michael Fuget <michaelfuget@gmail.com>
2020-12-02 07:38:23 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,9 +1,22 @@
 class Book
-  attr_accessor :author, :page_count, :genre
+  attr_accessor :author, :page_count
   attr_reader :title
-
+  
+  GENRES = []
+  
   def initialize(title)
     @title = title
+  end
+  
+  def genre=(book_genre)
+    if(!GENRES.include?(book_genre))
+      GENRES.push(book_genre)
+    end
+    @genre = book_genre
+  end 
+  
+  def genre
+    return @genre
   end
 
   def turn_page
